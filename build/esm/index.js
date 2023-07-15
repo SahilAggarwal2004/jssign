9b74f059fe918f8a0344660521a46f259a7e1452
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-07-15 23:14:16 +0530

    Fixed special characters problem

--- build/esm/index.js
+++ build/esm/index.js
@@ -1,7 +1,9 @@
 import sjcl from 'sjcl';
 const defaults = { v: 1, iter: 10000, ks: 128, ts: 64, mode: "ccm", adata: "", cipher: "aes" };
 const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', '~', '!', '@', '#', '$', '%', '^', '&'];
-const randomNumber = (min = 0, max = Number.MAX_SAFE_INTEGER) => min + Math.floor(Math.random() * (max - min + 1));
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+const randomNumber = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
 const randomElement = (array) => array[randomNumber(0, array.length - 1)];
 const textToChars = (text) => text.split('').map(c => c.charCodeAt(0));
 const byteHex = (n) => ("0" + Number(n).toString(16)).slice(-2);
@@ -12,25 +14,45 @@ function genSalt(length) {
         salt += randomElement(characters);
     return salt;
 }
-function encode(data, secret) {
-    const applySecret = (code) => textToChars(secret).reduce((a, b) => a ^ b, code);
-    return data.toString().split('').map(textToChars).map(applySecret).map(byteHex).join('');
+function encode(data, secret, type) {
+    if (type === 0) {
+        const applySecret = (code) => textToChars(secret).reduce((a, b) => a ^ b, code);
+        return data.toString().split('').map(textToChars).map(applySecret).map(byteHex).join('');
+    }
+    const dataBytes = encoder.encode(data);
+    const secretBytes = encoder.encode(secret);
+    const dataLength = dataBytes.length;
+    const secretLength = secretBytes.length;
+    const tokenBytes = new Uint8Array(dataLength);
+    for (let i = 0; i < dataLength; i++)
+        tokenBytes[i] = dataBytes[i] ^ secretBytes[i % secretLength];
+    return Array.from(tokenBytes).map(byte => byte.toString(16).padStart(2, '0')).join('');
 }
-function decode(token, secret) {
-    const applySecret = (code) => textToChars(secret).reduce((a, b) => a ^ b, code);
-    return token.match(/.{1,2}/g).map((hex) => parseInt(hex, 16)).map(applySecret).map(stringFromCode).join('');
+function decode(token, secret, type) {
+    if (type === 0) {
+        const applySecret = (code) => textToChars(secret).reduce((a, b) => a ^ b, code);
+        return token.match(/.{1,2}/g).map((hex) => parseInt(hex, 16)).map(applySecret).map(stringFromCode).join('');
+    }
+    const tokenBytes = new Uint8Array(token.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+    const secretBytes = encoder.encode(secret);
+    const tokenLength = tokenBytes.length;
+    const secretLength = secretBytes.length;
+    const decodedBytes = new Uint8Array(tokenLength);
+    for (let i = 0; i < tokenLength; i++)
+        decodedBytes[i] = tokenBytes[i] ^ secretBytes[i % secretLength];
+    return decoder.decode(decodedBytes);
 }
 function sign(data, secret, { expiresIn = 0, sl = 8 } = { expiresIn: 0, sl: 8 }) {
     const salt = genSalt(sl);
-    const token = encode(JSON.stringify({ data, iat: Date.now(), exp: expiresIn }), secret + salt);
-    const signature = encode(salt, secret);
+    const token = encode(JSON.stringify({ data, iat: Date.now(), exp: expiresIn }), salt, 1);
+    const signature = encode(salt, secret, 0);
     return `${token}.${signature}`;
 }
 function verify(token, secret) {
     try {
         const [dataStr, signature] = token.split('.');
-        const salt = decode(signature, secret);
-        const { data, iat, exp } = JSON.parse(decode(dataStr, secret + salt));
+        const salt = decode(signature, secret, 0);
+        const { data, iat, exp } = JSON.parse(decode(dataStr, salt, 1));
         if (!exp || Date.now() < iat + exp)
             return data;
         throw new Error();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import sjcl from 'sjcl'
 
+type Type = 0 | 1
+
 const defaults = { v: 1, iter: 10000, ks: 128, ts: 64, mode: "ccm", adata: "", cipher: "aes" }
 const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', '~', '!', '@', '#', '$', '%', '^', '&']
 
-const randomNumber = (min: number = 0, max: number = Number.MAX_SAFE_INTEGER) => min + Math.floor(Math.random() * (max - min + 1))
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+const randomNumber = (min: number, max: number) => min + Math.floor(Math.random() * (max - min + 1))
 const randomElement = (array: any[]) => array[randomNumber(0, array.length - 1)]
 const textToChars = (text: string) => text.split('').map(c => c.charCodeAt(0))
 const byteHex = (n: number) => ("0" + Number(n).toString(16)).slice(-2)
@@ -15,28 +20,47 @@ function genSalt(length: number) {
     return salt
 }
 
-function encode(data: string, secret: string) {
-    const applySecret = (code: any) => textToChars(secret).reduce((a, b) => a ^ b, code)
-    return data.toString().split('').map(textToChars).map(applySecret).map(byteHex).join('')
+function encode(data: string, secret: string, type: Type): string {
+    if (type === 0) {
+        const applySecret = (code: any) => textToChars(secret).reduce((a, b) => a ^ b, code)
+        return data.toString().split('').map(textToChars).map(applySecret).map(byteHex).join('')
+    }
+    const dataBytes = encoder.encode(data);
+    const secretBytes = encoder.encode(secret);
+    const dataLength = dataBytes.length
+    const secretLength = secretBytes.length
+    const tokenBytes = new Uint8Array(dataLength);
+    for (let i = 0; i < dataLength; i++) tokenBytes[i] = dataBytes[i] ^ secretBytes[i % secretLength];
+    return Array.from(tokenBytes).map(byte => byte.toString(16).padStart(2, '0')).join('');
 }
 
-function decode(token: any, secret: string) {
-    const applySecret = (code: any) => textToChars(secret).reduce((a, b) => a ^ b, code)
-    return token.match(/.{1,2}/g).map((hex: string) => parseInt(hex, 16)).map(applySecret).map(stringFromCode).join('')
+function decode(token: string, secret: string, type: Type): string {
+    if (type === 0) {
+        const applySecret = (code: any) => textToChars(secret).reduce((a, b) => a ^ b, code)
+        return token.match(/.{1,2}/g)!.map((hex: string) => parseInt(hex, 16)).map(applySecret).map(stringFromCode).join('')
+    }
+    const tokenBytes = new Uint8Array(token.match(/.{1,2}/g)!.map(byte => parseInt(byte, 16)));
+    const secretBytes = encoder.encode(secret);
+    const tokenLength = tokenBytes.length
+    const secretLength = secretBytes.length
+    const decodedBytes = new Uint8Array(tokenLength);
+    for (let i = 0; i < tokenLength; i++) decodedBytes[i] = tokenBytes[i] ^ secretBytes[i % secretLength];
+    return decoder.decode(decodedBytes);
 }
+
 
 function sign(data: any, secret: string, { expiresIn = 0, sl = 8 }: { expiresIn: number, sl: number } = { expiresIn: 0, sl: 8 }): string {
     const salt = genSalt(sl)
-    const token = encode(JSON.stringify({ data, iat: Date.now(), exp: expiresIn }), secret + salt)
-    const signature = encode(salt, secret)
+    const token = encode(JSON.stringify({ data, iat: Date.now(), exp: expiresIn }), salt, 1)
+    const signature = encode(salt, secret, 0)
     return `${token}.${signature}`
 }
 
 function verify(token: string, secret: string) {
     try {
         const [dataStr, signature] = token.split('.')
-        const salt = decode(signature, secret)
-        const { data, iat, exp } = JSON.parse(decode(dataStr, secret + salt))
+        const salt = decode(signature, secret, 0)
+        const { data, iat, exp } = JSON.parse(decode(dataStr, salt, 1))
         if (!exp || Date.now() < iat + exp) return data
         throw new Error()
     } catch { throw new Error('Invalid token or secret!') }
