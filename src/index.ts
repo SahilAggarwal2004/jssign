5b6e79044af39c3d7582e5c80612be7c42919524
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-12-29 18:06:54 +0530

    v0.2.6

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import sjcl from 'sjcl'
 
 type Type = 0 | 1
-export interface SignOptions {
-    expiresIn?: number
-    sl?: number
-}
-export interface EncryptOptions {
-    expiresIn?: number
-}
+
+export type EncryptOptions = { expiresIn?: number }
+
+export type SignOptions = EncryptOptions & { sl?: number }
 
 const defaults = { v: 1, iter: 10000, ks: 128, ts: 64, mode: "ccm", adata: "", cipher: "aes" }
 const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', ';', ',', '.', '/', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', ':', '<', '>', '?']
