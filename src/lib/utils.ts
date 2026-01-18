7c7b6e9881ac48916c8988afe7511a5755735143
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2026-01-18 18:04:56 +0530

    refactor: Simplify `genSalt` by picking characters directly from string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,11 @@ export const byteHex = (n: number): string => ("0" + Number(n).toString(16)).sli
 
 const randomNumber = (min: number, max: number) => min + Math.floor(Math.random() * (max - min + 1));
 
-export const randomElement = <T>(array: readonly T[]): T => array[randomNumber(0, array.length - 1)];
+const randomChar = (str: string) => str[randomNumber(0, str.length - 1)];
 
 export function genSalt(length: number) {
-  let salt = randomElement(characters);
-  for (let i = 1; i < length; i++) salt += randomElement(characters);
+  let salt = randomChar(characters);
+  for (let i = 1; i < length; i++) salt += randomChar(characters);
   return salt;
 }
 
